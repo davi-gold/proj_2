fc624bc46227f3d420994135458535f2679ab7cc
David <offirgold@gmail.com>
2019-01-07 00:46:37 +0200

    StringReverser #1

--- StringReverser.cpp
+++ StringReverser.cpp
@@ -8,6 +8,6 @@ string StringReverser::solve(string problem) {
     int len = problem.length();
     for (int i = 0; i < len / 2; i++)
         swap(problem[i], problem[len - i - 1]);
-    return problem;
+    return proble;
 }
 
