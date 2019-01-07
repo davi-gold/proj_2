24ed9a941920e0f197780610048c97fe60156e32
David <offirgold@gmail.com>
2019-01-08 01:03:26 +0200

    time out #1

--- MySerialServer.cpp
+++ MySerialServer.cpp
@@ -18,31 +18,44 @@ void MySerialServer::open(int port, ClientHandler *c) {
     bind(sockfd, (struct sockaddr *) &address, sizeof(address));
     mySockfd = sockfd;
     listen(sockfd, 5);
-//    listenToClient(sockfd,address,addrlen,c);
+
     thread first(listenToClient, sockfd, address, addrlen, c);
     first.detach();
+}
 
-
-    //WILL OPEN A THREAD HERE
-
+void MySerialServer::stop() {
+    close(mySockfd);
 }
 
 void MySerialServer::listenToClient(int sockfd, struct sockaddr_in address, int addrlen, ClientHandler *c) {
     int new_socket;
 
-    while (true) {
-        if ((new_socket = accept(sockfd, (struct sockaddr *) &address,
-                                 (socklen_t *) &addrlen)) < 0) {
-            perror("error");
-            exit(EXIT_FAILURE);
+    bool flag = true;
+    while (flag) {
+        bool timeOut = true;
+        // using lambda function - t1 => new thread
+        std::thread t1([&]() {
+            new_socket = accept(sockfd, (struct sockaddr *) &address, (socklen_t *) &addrlen);
+
+            if (new_socket < 0) { // if ACCEPT fails
+                perror("error");
+                exit(EXIT_FAILURE);
+            }
+            // if ACCEPT succeeded - continue without Time-Out
+            timeOut = false;
+        });
+
+        // allow execution to continue independently by 'detach'
+        t1.detach();
+
+        // the main is waiting 5 seconds and then checks the variable 'timeOut'
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+
+        // if ACCEPT failed after 5 seconds --> Time-Out
+        if (timeOut) {
+            cout << "<<< TIME OUT >>>" << endl;
+            flag = false; // exiting while loop
         }
-
         c->handleClient(new_socket);
-        //sleep here??
-        //this_thread::sleep_for(chrono::milliseconds(int(250)));
     }
-}
-
-void MySerialServer::stop() {
-    close(mySockfd);
 }
