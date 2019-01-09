ac694dc3b2f90873791469ff1896adfbfd97f76d
David <offirgold@gmail.com>
2019-01-09 15:41:14 +0200

    search

---
+++ BestFirstSearch.cpp
@@ -0,0 +1,27 @@
+//
+// Created by david on 1/9/19.
+//
+
+#include "BestFirstSearch.h"
+template<class P, class S, class T>
+
+S BestFirstSearch::search(Searchable<T> searchable) {
+
+        addToOpenList(searchable. getInitialState ()); // inherited from Searcher
+        HashSet<State> closed = new HashSet<State>();
+        while (OpenListSize > 0) {
+            State n = popOpenList(); // inherited from Searcher, removes the best state
+            closed.Add(n);
+            if (n.Equals(searchable. getIGoallState ()))
+                return backTrace(); // private method, back traces through the parents
+// calling the delegated method, returns a list of states with n as a parent
+            List<State> succerssors = searchable. getAllPossibleStates(n);
+            foreach (State s in succerssors)
+            {
+                if (!closed.Contains(s) && !openContaines(s))
+                {
+// s.setCameFrom(n); // already done by getSuccessors
+                    addToOpenList(s);
+                }
+                else
+}
