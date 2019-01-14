e7573eb93279f4d18b4f62c5113c87b96ebab65b
David <offirgold@gmail.com>
2019-01-14 23:59:21 +0200

    astar

--- Astar.h
+++ Astar.h
@@ -5,13 +5,15 @@
 #ifndef PROJ_2_ASTAR_H
 #define PROJ_2_ASTAR_H
 
+#include <unordered_set>
 #include "ISearchable.h"
 #include "Searcher.h"
 
 template<class S, class T>
 class Astar : public Searcher<S, T> {
     S search(ISearchable<T> *searchable) {
-        unordered_set < State<T> * > *closed;
+        int g, f, h;
+        unordered_set<State<T> *> *closed;
         State<T> *s = searchable->getInitialState();
         s->setCameFrom(NULL);
         this->openList->push(s); // Add the start node
@@ -20,11 +22,37 @@ class Astar : public Searcher<S, T> {
         while (!this->openList->getQueue().empty() && flag == true) {
             State<T> *n = this->openList->popAndGet();
             this->evalNodes++;
+            closed->insert(n);
 
+            list<State<T> *> successors = searchable->getAllPossibleStates(n); // Create n's successors
+            for (typename list<State<T> *>::iterator it = successors.begin(); it != successors.end(); ++it) {
+                State<T> *s = *it; // current node
+                if (closed->find(s) != closed->end())
+                    break;
+                else {
+                    if (!this->openList->find(s)) {
+                        this->openList->push(s);
+                        s->setCameFrom(n);
+                        g = s->getCostPath();
+                        // record the path
+                        h = 0;
+                        f = g + h;
+                    } else {
+
+                    }
+                }
+                // generate relevant path
+                double thisPath = n->getCostPath() + s->getCost();
+
+
+            }
+        }
+
+
+        State<T> *getLowestFcost(MyPriorityQueue<T> *q) {
 
         }
-    }
-};
+    };
 
 
 #endif //PROJ_2_ASTAR_H
