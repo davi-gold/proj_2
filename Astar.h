1ed2cefd704c770f46ca10b3ee93f3deac2ec8f7
David <offirgold@gmail.com>
2019-01-15 12:14:41 +0200

    bestfirst

--- Astar.h
+++ Astar.h
@@ -1,58 +1,58 @@
+////
+//// Created by david on 1/14/19.
+////
 //
-// Created by david on 1/14/19.
+//#ifndef PROJ_2_ASTAR_H
+//#define PROJ_2_ASTAR_H
 //
-
-#ifndef PROJ_2_ASTAR_H
-#define PROJ_2_ASTAR_H
-
-#include <unordered_set>
-#include "ISearchable.h"
-#include "Searcher.h"
-
-template<class S, class T>
-class Astar : public Searcher<S, T> {
-    S search(ISearchable<T> *searchable) {
-        int g, f, h;
-        unordered_set<State<T> *> *closed;
-        State<T> *s = searchable->getInitialState();
-        s->setCameFrom(NULL);
-        this->openList->push(s); // Add the start node
-        searchable->getInitialState()->setVisited(true);
-        bool flag = true;
-        while (!this->openList->getQueue().empty() && flag == true) {
-            State<T> *n = this->openList->popAndGet();
-            this->evalNodes++;
-            closed->insert(n);
-
-            list<State<T> *> successors = searchable->getAllPossibleStates(n); // Create n's successors
-            for (typename list<State<T> *>::iterator it = successors.begin(); it != successors.end(); ++it) {
-                State<T> *s = *it; // current node
-                if (closed->find(s) != closed->end())
-                    break;
-                else {
-                    if (!this->openList->find(s)) {
-                        this->openList->push(s);
-                        s->setCameFrom(n);
-                        g = s->getCostPath();
-                        // record the path
-                        h = 0;
-                        f = g + h;
-                    } else {
-
-                    }
-                }
-                // generate relevant path
-                double thisPath = n->getCostPath() + s->getCost();
-
-
-            }
-        }
-
-
-        State<T> *getLowestFcost(MyPriorityQueue<T> *q) {
-
-        }
-    };
-
-
-#endif //PROJ_2_ASTAR_H
+//#include <unordered_set>
+//#include "ISearchable.h"
+//#include "Searcher.h"
+//
+//template<class S, class T>
+//class Astar : public Searcher<S, T> {
+//    S search(ISearchable<T> *searchable) {
+//        int g, f, h;
+//        unordered_set<State<T> *> *closed;
+//        State<T> *s = searchable->getInitialState();
+//        s->setCameFrom(NULL);
+//        this->openList->push(s); // Add the start node
+//        searchable->getInitialState()->setVisited(true);
+//        bool flag = true;
+//        while (!this->openList->getQueue().empty() && flag == true) {
+//            State<T> *n = this->openList->popAndGet();
+//            this->evalNodes++;
+//            closed->insert(n);
+//
+//            list<State<T> *> successors = searchable->getAllPossibleStates(n); // Create n's successors
+//            for (typename list<State<T> *>::iterator it = successors.begin(); it != successors.end(); ++it) {
+//                State<T> *s = *it; // current node
+//                if (closed->find(s) != closed->end())
+//                    break;
+//                else {
+//                    if (!this->openList->find(s)) {
+//                        this->openList->push(s);
+//                        s->setCameFrom(n);
+//                        g = s->getCostPath();
+//                        // record the path
+//                        h = 0;
+//                        f = g + h;
+//                    } else {
+//
+//                    }
+//                }
+//                // generate relevant path
+//                double thisPath = n->getCostPath() + s->getCost();
+//
+//
+//            }
+//        }
+//
+//
+//        State<T> *getLowestFcost(MyPriorityQueue<T> *q) {
+//
+//        }
+//    };
+//
+//
+//#endif //PROJ_2_ASTAR_H
--- BestFirstSearch.h
+++ BestFirstSearch.h
@@ -11,59 +11,67 @@
 
 template<class S, class T>
 
-class BestFirstSearch : public Searcher <S, T>{
+class BestFirstSearch : public Searcher<S, T> {
 public:
-     virtual S search(ISearchable<T> *searchable){
-         State<T> *s = searchable->getInitialState();
-         this->openList->push(s); // OPEN = [initial state] ::: a priority queue of states to be evaluated
-         s->setCameFrom(NULL);
-         searchable->getInitialState()->setVisited(true);
-         unordered_set<State<T> *> *closed = new unordered_set<State<T>*>(); // CLOSED = [] ::: a set of states already evaluated
-         vector<State<T> *> pVec;
-         bool flag = true;
-         while (!(this->openList->getQueue()->empty()) && flag) {
-             State<T> *n = this->openList->popAndGet(); // n <-- dequeue(OPEN) ::: Remove the best node from OPEN
-             this->evalNodes++;
-             closed->insert(n); // add(n,CLOSED) ::: so we won’t check n again
+    virtual S search(ISearchable<T> *searchable) {
+        State<T> *s = searchable->getInitialState();
+        this->openList->push(s); // OPEN = [initial state] ::: a priority queue of states to be evaluated
+        s->setCameFrom(NULL);
+        searchable->getInitialState()->setVisited(true);
+        unordered_set<State<T> *> *closed = new unordered_set<State<T> *>(); // CLOSED = [] ::: a set of states already evaluated
+        vector<State<T> *> pVec;
+        bool flag = true;
+        while (!(this->openList->getQueue()->empty()) && flag) {
+            State<T> *n = this->openList->popAndGet(); // n <-- dequeue(OPEN) ::: Remove the best node from OPEN
+            this->evalNodes++;
+            closed->insert(n); // add(n,CLOSED) ::: so we won’t check n again
 
-             if (searchable->isGoalState(n)) { // If n is the goal state
-                 //*pVec = searchable->backTrace();
-                 pVec = searchable->backTrace(n); // back traces through the parents, calling the delegated method, returns a list of states with n as a parent
-                 flag = false;
-             }
-             if (flag) {
-                 int evalNodes = this->getNumOfNodesEval();
-                 evalNodes++;
-                 this->setEvaluatedNodes(evalNodes);
-                 list<State<T> *> successors = searchable->getAllPossibleStates(n); // Create n's successors
-                 for (typename list<State<T>*>::iterator it = successors.begin(); it != successors.end(); ++it) {
-                     State<T> *s = *it;
-                     // generate relevant path
-                     double thisPath = n->getCostPath() + s->getCost();
+            if (searchable->isGoalState(n)) { // If n is the goal state
+                //*pVec = searchable->backTrace();
+                pVec = searchable->backTrace(
+                        n); // back traces through the parents, calling the delegated method, returns a list of states with n as a parent
+                flag = false;
+            }
+            if (flag) {
+                int evalNodes = this->getNumOfNodesEval();
+                evalNodes++;
+                this->setEvaluatedNodes(evalNodes);
+                list<State<T> *> successors = searchable->getAllPossibleStates(n); // Create n's successors
+                for (typename list<State<T> *>::iterator it = successors.begin(); it != successors.end(); ++it) {
+                    State<T> *s = *it;
+                    // generate relevant path
+                    double thisPath = n->getCostPath() + s->getCost();
 
-                     // if it is not in CLOSED and it is not in OPEN
-                     //*!closed->find(s)
-                     if (closed->find(s)!=closed->end() && !this->openList->find(s)) {
-                         s->setCameFrom(n);
-                         s->setCostPath(thisPath);
-                         this->openList->push(s);
-                     } else if (thisPath < s->getCostPath()) {
-                         // Otherwise, if this new path is better than previous one
-                         if (!this->openList->find(s))
-                             this->openList->push(s);
-                         else
-                             this->openList->updatePrior(s, n);
-                     }
-                 }
-             }
-         }
-         if (!pVec.empty())
-             return searchable->getDirections(pVec);
-         else
-             //return perror("path is empty!\n");
-             throw ("path is empty!\n");
-     }
+                    // if it is not in CLOSED and it is not in OPEN
+                    //*!closed->find(s)
+                    if (!checkIfInClosed(s, closed) && !this->openList->find(s)) {
+                        s->setCameFrom(n);
+                        s->setCostPath(thisPath);
+                        this->openList->push(s);
+                    } else if (thisPath < s->getCostPath()) {
+                        // Otherwise, if this new path is better than previous one
+                        if (!this->openList->find(s))
+                            this->openList->push(s);
+                        else
+                            this->openList->updatePrior(s, n);
+                    }
+                }
+            }
+        }
+        if (!pVec.empty())
+            return searchable->getDirections(pVec);
+        else
+            //return perror("path is empty!\n");
+            throw ("path is empty!\n");
+    }
 };
 
+template<class T>
+bool checkIfInClosed(State<T> *state, unordered_set<State<T> *> *closed) {
+    if (closed->find(state) == closed->end())
+        return false;
+    else
+        return true;
+}
 
 #endif //PROJ_2_BESTFIRSTSEARCH_H
